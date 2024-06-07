c0edb0db1ce2aebb38da9059584cfea7eb9d57f8
Jonathan Ouyang <109103876+JonOuyang@users.noreply.github.com>
2024-06-06 21:36:20 -0700

    Create Permutation.java

---
+++ Stacks and Queues/Deques and Randomized Queues/Permutation.java
@@ -0,0 +1,14 @@
+import edu.princeton.cs.algs4.StdIn;
+
+public class Permutation {
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        RandomizedQueue<String> queue = new RandomizedQueue<>();
+        while (!StdIn.isEmpty()) {
+            queue.enqueue(StdIn.readString());
+        }
+        for (int i = 0; i < num; i++) {
+            System.out.println(queue.dequeue());
+        }
+    }
+}
